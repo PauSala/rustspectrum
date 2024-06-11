80e1ceba98cce236421d37911c583528612bf3a2
pausala <pau.salaf@gmail.com>
2024-06-11 19:21:43 +0200

    moz

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,160 @@
-use std::collections::VecDeque;
-use std::fs::File;
-use std::io::BufReader;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::{Arc, Mutex};
-use std::time::{Duration, SystemTime};
-
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{BufferSize, StreamConfig};
 use minifb::{Key, Window, WindowOptions};
+use rodio::Decoder;
+use rodio::{OutputStream, Source};
 use rustfft::num_complex::Complex;
 use rustfft::FftPlanner;
+use std::fs::File;
+use std::io::BufReader;
+use std::time::SystemTime;
 
-const WIDTH: usize = 1024;
-const HEIGHT: usize = 812;
+const WIDTH: usize = 512;
+const HEIGHT: usize = 512;
 const DELTA: f32 = 8.0;
 const CHUNK_SIZE: usize = 8192;
-const FREQS_SIZE: usize = 128;
+const SHRINK_FACTOR: usize = 8;
 
-fn main() {
-    let host = cpal::default_host();
-    let device = host
-        .default_output_device()
-        .expect("no output device available");
-    let mut supported_configs_range = device
-        .supported_output_configs()
-        .expect("error while querying configs");
-    let config = supported_configs_range
-        .next()
-        .expect("no supported config?!")
-        .with_max_sample_rate();
-    println!("{:?}", &config);
-    let config: StreamConfig = StreamConfig {
-        channels: config.channels(),
-        sample_rate: config.sample_rate(),
-        buffer_size: BufferSize::Default,
-    };
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open("moz.mp3").unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file)?.buffered().amplify(1.);
 
-    let file = BufReader::new(File::open("bach.wav").expect("Failed to open audio file"));
-    let mut reader = hound::WavReader::new(file).expect("Failed to read WAV file");
-    let mut original_samples: Vec<f32> = reader
-        .samples::<i16>()
-        .map(|s| s.expect("Failed to read sample") as f32 / i16::MAX as f32)
+    // Collect samples
+    let sample_rate = source.sample_rate();
+    println!("Sample rate: {}", sample_rate);
+    let samples: Vec<f32> = source.clone().convert_samples().collect();
+
+    let (left_channel, right_channel): (Vec<(usize, &f32)>, Vec<(usize, &f32)>) = samples
+        .iter()
+        .enumerate()
+        .partition(|(index, _sample)| index % 2 == 0);
+
+    let left_channel: Vec<f32> = left_channel
+        .into_iter()
+        .map(|(_index, sample)| *sample)
+        .collect();
+    let right_channel: Vec<f32> = right_channel
+        .into_iter()
+        .map(|(_index, sample)| *sample)
         .collect();
 
-    apply_hann_window(&mut original_samples);
-    let mut freqs: Vec<f32> = Vec::new();
-    for chunk in original_samples.chunks(CHUNK_SIZE) {
-        let frequencies = analyze_frequencies(chunk);
-        for _ in 0..CHUNK_SIZE / FREQS_SIZE {
-            for f in &frequencies[0..FREQS_SIZE] {
-                freqs.push(*f);
-            }
-        }
+    let duration = left_channel.len() as f64 / sample_rate as f64;
+    println!("Duration: {}", duration);
+
+    // Process the samples in chunks (e.g., 1024 samples per chunk)
+
+    let mut l_sample_chunks = left_channel.chunks(CHUNK_SIZE);
+    let mut r_sample_chunks = right_channel.chunks(CHUNK_SIZE);
+
+    let mut freqs: Vec<Vec<f32>> = Vec::new();
+    while let Some(chunk) = l_sample_chunks.next() {
+        let mut clonedl = chunk.to_vec();
+        let mut clonedr = r_sample_chunks.next().unwrap().to_vec();
+        apply_hann_window(&mut clonedl);
+        apply_hann_window(&mut clonedr);
+        let l_frequencies = analyze_frequencies(&clonedl, sample_rate);
+        let r_frequencies = analyze_frequencies(&clonedr, sample_rate);
+        let frequencies: Vec<f32> = l_frequencies
+            .iter()
+            .zip(r_frequencies.iter())
+            .map(|(l, r)| (l + r) / 2.0)
+            .collect();
+        freqs.push(frequencies);
     }
+    //freqs = process_freqs(&freqs);
     normalize_freqs(&mut freqs);
-    let samples_len = original_samples.len();
-    println!("samples len: {} | freqs: {}", samples_len, freqs.len());
-    let freqs_index = Arc::new(AtomicUsize::new(0));
-    let freqs_index_clone = Arc::clone(&freqs_index);
 
-    // Arc and Mutex for thread-safe data sharing
-    // Use VecDeque for efficient front removal
-    let samples = Arc::new(Mutex::new(VecDeque::from(original_samples.clone())));
-    let samples_clone = Arc::clone(&samples);
+    let colors = gradient(CHUNK_SIZE / SHRINK_FACTOR / 2);
 
-    //let mut now = std::time::Instant::now();
-
-    let output_stream = device
-        .build_output_stream(
-            &config,
-            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-                let mut samples = samples_clone.lock().unwrap();
-                for sample in data.iter_mut() {
-                    if let Some(s) = samples.pop_front() {
-                        *sample = s;
-                    } else {
-                        *sample = 0.0;
-                    }
-                }
-                freqs_index_clone.fetch_add(data.len(), Ordering::Relaxed);
-            },
-            move |err| {
-                eprintln!("an error occurred on stream: {}", err);
-            },
-            Some(Duration::from_millis(0)),
-        )
-        .expect("error while building stream");
-    output_stream.play().expect("error while playing stream");
-
-    println!("Playing audio");
     // Create a window for visualization
     let mut window = Window::new(
         "Frequency Spectrum",
         WIDTH,
         HEIGHT,
         WindowOptions::default(),
-    )
-    .unwrap();
+    )?;
 
+    let chunks_per_milisecond = freqs.len() as f64 / (duration * 1000.0);
+
+    //window.set_target_fps(left_channel.len() / duration as usize / chunk_size as usize);
+    let mut i = 0;
+    let all_start = SystemTime::now();
     let mut start = SystemTime::now();
-    let mut curr = vec![0.0; FREQS_SIZE];
-    let colors = gradient(FREQS_SIZE);
-
-    let mut prev_index = 0;
-
-    while window.is_open()
-        && !window.is_key_down(Key::Escape)
-        && freqs_index.load(Ordering::Relaxed) < freqs.len() - 1024
-    {
+    let mut chunk_count = 0.0;
+    // Play the audio
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+    stream_handle.play_raw(source.convert_samples())?;
+    let mut curr = vec![0.0; freqs[0].len() / SHRINK_FACTOR];
+    while window.is_open() && !window.is_key_down(Key::Escape) && i < freqs.len() {
         let end = SystemTime::now();
-        let index = freqs_index.load(Ordering::Acquire);
-        if index == prev_index {
-            continue;
-        }
-        let millis = end.duration_since(start).unwrap().as_millis() as f64;
-        let freqs = freqs[index..index + FREQS_SIZE].to_vec();
-        // Update the curr vector based on freqs and millis
-        for i in 0..freqs.len() {
-            curr[i] += (freqs[i] - curr[i]) as f32 * (millis / 1000.0) as f32 * DELTA;
-        }
-        // println!("Freqs {:?}", &curr);
-        // Visualize the curr vector
-        let v = visualize_bars(&curr, &colors);
-        // Update the window's buffer with the visualization
-        window.update_with_buffer(&v, WIDTH, HEIGHT).unwrap();
+        let elapsed = end.duration_since(start).unwrap();
+        let millis = elapsed.as_nanos() as f64 / 1_000_000.0;
+        chunk_count += millis * chunks_per_milisecond;
 
-        // Update the start time for the next iteration
+        if i < freqs.len() {
+            let freq = freqs.get(i).unwrap();
+            for i in 0..&freq.len() / SHRINK_FACTOR {
+                curr[i] += (freq[i] - curr[i]) as f32 * (millis / 1000.0) as f32 * DELTA;
+            }
+            let b = visualize_frequencies(&curr, &colors);
+            window.update_with_buffer(&b, WIDTH, HEIGHT)?;
+        }
+        i += 1;
+        if i != chunk_count.round() as usize {
+            i = chunk_count.round() as usize;
+        }
         start = end;
-        prev_index = index;
     }
 
-    println!("Finished playing");
+    let end = SystemTime::now();
+    match end.duration_since(all_start) {
+        Ok(elapsed) => {
+            println!(
+                "Total time: {} duration {}",
+                elapsed.as_millis() as f32 / 1_000.,
+                duration
+            );
+        }
+        Err(e) => {
+            // An error occurred!
+            println!("Error: {:?}", e);
+        }
+    }
+    Ok(())
+}
+
+fn normalize_freqs(freqs: &mut Vec<Vec<f32>>) {
+    let mut min = f32::MAX;
+    let mut max = f32::MIN;
+
+    // Apply a logarithmic function to compress the range
+    for row in freqs.iter_mut() {
+        for value in row.iter_mut() {
+            *value = (*value + 1.0).ln();
+        }
+    }
+
+    // Find the minimum and maximum values in freqs
+    for row in freqs.iter() {
+        for &value in row.iter() {
+            if value > max {
+                max = value;
+            }
+            if value < min {
+                min = value;
+            }
+        }
+    }
+
+    // Normalize the values in freqs to be between 0 and
+    for row in freqs.iter_mut() {
+        for value in row.iter_mut() {
+            *value = (*value - min) / (max - min);
+            if *value == 1.0 {
+                println!("Max value: {}", max);
+            }
+        }
+    }
 }
 
 fn hann_window(n: usize) -> Vec<f32> {
@@ -151,7 +175,7 @@ fn apply_hann_window(samples: &mut [f32]) {
     }
 }
 
-fn analyze_frequencies(samples: &[f32]) -> Vec<f32> {
+fn analyze_frequencies(samples: &[f32], _sample_rate: u32) -> Vec<f32> {
     let mut planner = FftPlanner::<f32>::new();
     let fft = planner.plan_fft_forward(samples.len());
     let mut spectrum: Vec<Complex<f32>> = samples
@@ -162,23 +186,19 @@ fn analyze_frequencies(samples: &[f32]) -> Vec<f32> {
     let half = spectrum.len() / 2;
     spectrum
         .iter()
-        .skip(1)
         .take(half)
         .map(|sample| sample.norm())
         .collect() // Only take the first half of the spectrum
 }
 
-fn visualize_bars(frequencies: &[f32], colors: &Vec<u32>) -> Vec<u32> {
-    let mut buffer: Vec<u32> = vec![0x0f0015; WIDTH * HEIGHT];
+fn _visualize_frequencies(frequencies: &[f32], colors: &Vec<u32>) -> Vec<u32> {
+    let mut buffer: Vec<u32> = vec![0x1c0424; WIDTH * HEIGHT];
     let margin = 1;
     let sample_width = (WIDTH / frequencies.len()) - margin;
     for (i, &sample) in frequencies.iter().enumerate() {
         let x = i * (sample_width + margin);
         let y = ((1.0 - sample) * HEIGHT as f32) as usize; // flip the visualization
-        if y == 0 {
-            println!("y: {}", y);
-        }
-        let color = colors[i % colors.len()];
+        let color = colors[i % colors.len()]; // create a color based on the index
         for j in x..x + sample_width {
             for k in y..HEIGHT {
                 let index = j + k * WIDTH;
@@ -188,17 +208,18 @@ fn visualize_bars(frequencies: &[f32], colors: &Vec<u32>) -> Vec<u32> {
             }
         }
     }
+
     buffer
 }
 
-fn visualize_circle(frequencies: &[f32], colors: &Vec<u32>) -> Vec<u32> {
-    let mut buffer: Vec<u32> = vec![0x0f0015; WIDTH * HEIGHT];
+pub fn visualize_frequencies(frequencies: &[f32], colors: &Vec<u32>) -> Vec<u32> {
+    let mut buffer: Vec<u32> = vec![0x1c0424; WIDTH * HEIGHT];
     let center_x = WIDTH / 2;
     let center_y = HEIGHT / 2;
     let radius = center_x.min(center_y) as f32;
 
     for (i, &sample) in frequencies.iter().enumerate() {
-        let angle = (i as f32 / frequencies.len() as f32) * std::f32::consts::PI;
+        let angle = (i as f32 / frequencies.len() as f32) * 2.0 * std::f32::consts::PI;
         let r = radius * sample;
         for r in 0..(r * 1000.) as usize {
             let x = (center_x as f32 + (r / 1000) as f32 * angle.cos()) as usize; // convert polar to cartesian coordinates
@@ -213,32 +234,6 @@ fn visualize_circle(frequencies: &[f32], colors: &Vec<u32>) -> Vec<u32> {
     }
 
     buffer
-}
-
-fn normalize_freqs(freqs: &mut Vec<f32>) {
-    let mut min = f32::MAX;
-    let mut max = f32::MIN;
-
-    // for value in freqs.iter_mut() {
-    //     *value = (*value + 1.0).ln();
-    // }
-
-    // Find the minimum and maximum values in freqs
-    for &value in freqs.iter() {
-        if value > max {
-            max = value;
-        }
-        if value < min {
-            min = value;
-        }
-    }
-
-    println!("Min: {} | Max: {}", min, max);
-
-    // Normalize the values in freqs to be between 0 and 1
-    for value in freqs.iter_mut() {
-        *value = (*value - min) / (max - min);
-    }
 }
 
 fn gradient(len: usize) -> Vec<u32> {
